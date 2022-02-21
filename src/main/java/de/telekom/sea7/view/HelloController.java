4dff24f3fffd7aa159388282299f9dd544b71a16
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-02-21 14:56:56 +0100

    20220221 ThymeLeafSeite läuft

--- src/main/java/de/telekom/sea7/view/HelloController.java
+++ src/main/java/de/telekom/sea7/view/HelloController.java
@@ -1,18 +1,17 @@
 package de.telekom.sea7.view;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
+
 @Controller
-public class ThymeLeafSeite {  
-    @GetMapping({"/", "/hello"})
+public class HelloController {  
+    @GetMapping("/")
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
-        return "hello";
+        return "ThymeLeafSeite";
     }
 	
 	}
