ca8059b71d0d94aeeba3288ce2754c3faceec071
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-02-21 16:10:24 +0100

    tets

--- src/main/java/de/telekom/sea7/view/TransactionView.java
+++ src/main/java/de/telekom/sea7/view/TransactionView.java
@@ -24,12 +24,12 @@ public class TransactionView {
 				+   "<meta charset=\"UTF-8\">"
 				+ "</head>"
 				+ "<body>"
-				+     "<h1>BANCO del LOSTA</h1>"
+				+     "<h1>Banco del Losta</h1>"
 				+     "<p>2022/02/15</p>"
 				+     "<h2>Losta Monetas</h2>"
 				+     "<p>Futschi Kato</p>"
 				+     "<p> Olla </p>"
-				+     "<p> bei Fragen bitte eine Mail an Niemanden: </p>"
+				+     "<p> bei Fragen bitte eine Mail an Niemanden! </p>"
 				+ "</body>"
 				+"</html>";
 		return html;
