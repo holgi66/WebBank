90f99f64968cae36b120f7f5bef4560cea36a932
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-02-21 11:23:46 +0100

    20220221 einbau ThymeLeaf

--- src/main/java/de/telekom/sea7/services/TransactionClass.java
+++ src/main/java/de/telekom/sea7/services/TransactionClass.java
@@ -1,14 +1,14 @@
 package de.telekom.sea7.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import de.telekom.sea7.model.Transaction;
 
 @Service
 public class TransactionClass {
-	@Autowired
-	private Transaction transaction;
-
+	
+	public Transaction transaction;
+@Bean
 	public Transaction getTransaction() {
 		return transaction;
 	}
