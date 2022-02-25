93a407e2de13912808fe3724760ef4b1fe2addd1
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-02-25 08:38:14 +0100

    20220225 neuer Tag neues Glueck

--- src/main/java/de/telekom/sea7/persistence/RepositoryTransactionImpl.java
+++ src/main/java/de/telekom/sea7/persistence/RepositoryTransactionImpl.java
@@ -1,14 +1,21 @@
 package de.telekom.sea7.persistence;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.telekom.sea7.impl.model.RepositoryIbanImpl;
+import de.telekom.sea7.impl.model.RepositoryReceiverImpl;
+import de.telekom.sea7.inter.model.Iban;
+import de.telekom.sea7.inter.model.Receiver;
+import de.telekom.sea7.inter.model.Repository;
 import de.telekom.sea7.model.Transaction;
 
 
@@ -16,6 +23,7 @@ public class RepositoryTransactionImpl extends DbConnection  {
 	
 	private String sqlForGetAll = "SELECT * FROM transactions";
 	private String sqlForGet = "SELECT * FROM transactions WHERE ID = ?";
+	private String sqlForAdd = "INSERT INTO transactions (receiver_ID, iban_ID, amount, purpose, date) VALUES (?, ?, ? ,? ,?)";
 	
 	
 	private Connection connection;
@@ -59,9 +67,30 @@ public class RepositoryTransactionImpl extends DbConnection  {
 		return (Transaction) psForGet;
 
 		}
+	
+	
+public void add(Transaction transaction) {
+	try {
+				
+		BigDecimal value = new BigDecimal(Float.toString(transaction.getAmount()));
+		
+		psForAdd.setInt(1, transaction.getReceiver().getId());
+		psForAdd.setInt(2, transaction.getIban().getId());
+		psForAdd.setBigDecimal(3, value);
+		psForAdd.setString(4, transaction.getPurpose());
+		psForAdd.setTimestamp(5, Timestamp.valueOf(transaction.getDate()));
+		psForAdd.execute();
+		ResultSet rs = psForAdd.getGeneratedKeys();
+		
+		if (rs.next()) {
+			transaction.setId(rs.getInt(1));
+		}
 	}
-	
-	
+	catch (SQLException e) {
+		System.out.println(e.getMessage());
+	}
+}
+}
 
 	
 
