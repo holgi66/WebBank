a998b17b175c4731d66060d3c840417f698c1e45
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-02-28 15:17:00 +0100

    20220228 kurze Pause

--- src/main/java/de/telekom/sea7/services/TransactionService.java
+++ src/main/java/de/telekom/sea7/services/TransactionService.java
@@ -1,10 +1,9 @@
 package de.telekom.sea7.services;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 
-@Component
+
 @Service
 public class TransactionService {
 	
