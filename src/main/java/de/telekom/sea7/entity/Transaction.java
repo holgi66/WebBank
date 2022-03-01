3b0fee7942fea200d65f8af4064d4bb657a1e239
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-01 16:29:39 +0100

    20220301 ende

--- src/main/java/de/telekom/sea7/entity/Transaction.java
+++ src/main/java/de/telekom/sea7/entity/Transaction.java
@@ -1,8 +1,5 @@
 package de.telekom.sea7.entity;
 
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,11 +11,6 @@ public class Transaction {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private int receiver_ID;
-	private int iban_ID;
-	private BigDecimal amount;
-	private String purpose;
-	private Timestamp date;
 
 	public Transaction() {
 	}
