fdaec806832ec1fed9bbd16f5b44b533932ea91d
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-02 13:23:37 +0100

    20220302 IbanRepository erstellt

---
+++ src/main/java/de/telekom/sea7/repository/IbanRepository.java
@@ -0,0 +1,11 @@
+package de.telekom.sea7.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import de.telekom.sea7.entity.Iban;
+
+@Repository
+public interface IbanRepository extends JpaRepository<Iban, Long> {
+
+}
