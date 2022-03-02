683d5dc15a283b36786c5c4f61d0cba7065d5166
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-02 11:06:03 +0100

    20220302 geziehlte abfrage ok!

---
+++ src/main/java/de/telekom/sea7/entity/Transactions.java
@@ -0,0 +1,74 @@
+package de.telekom.sea7.entity;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Transactions {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private int receiver_ID;
+	private int iban_ID;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getIban_ID() {
+		return iban_ID;
+	}
+
+	public void setIban_ID(int iban_ID) {
+		this.iban_ID = iban_ID;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public String getPurpose() {
+		return purpose;
+	}
+
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+
+	public Timestamp getDate() {
+		return date;
+	}
+
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+
+	private BigDecimal amount;
+	private String purpose;
+	private Timestamp date;
+
+	public Transactions() {
+	}
+
+	public int getReceiver_ID() {
+		return receiver_ID;
+	}
+
+	public void setReceiver_ID(int receiver_ID) {
+		this.receiver_ID = receiver_ID;
+	}
+
+}
--- src/main/java/de/telekom/sea7/repository/TransactionRepository.java
+++ src/main/java/de/telekom/sea7/repository/TransactionRepository.java
@@ -2,9 +2,9 @@ package de.telekom.sea7.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import de.telekom.sea7.entity.Transaction;
+import de.telekom.sea7.entity.Transactions;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transactions, Long> {
 
 }
