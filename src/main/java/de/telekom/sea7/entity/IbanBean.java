aad4795f302b8e3306715b6b0249e177f91106d1
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-03 11:45:12 +0100

    20220303 push

--- src/main/java/de/telekom/sea7/entity/IbanBean.java
+++ src/main/java/de/telekom/sea7/entity/IbanBean.java
@@ -12,30 +12,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class IbanBean implements ApplicationContextAware {
 
-	@Autowired private Iban iban;
-	
+	@Autowired
+	private Iban iban;
+
 	private Iban ibanEnd;
-	
+
 	private ApplicationContext applicationContext;
-	
+
 	public IbanBean() {
 		System.out.println(iban);
 	}
-	
+
 	@PostConstruct
 	public void init() {
 		System.out.println("#### IbanBean startet ####");
 		System.out.println(iban);
 	}
-	
+
 	@PreDestroy
 	public void destroy() {
 		System.out.println("#### IbanBean endet ####");
 	}
-	
+
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
-		ibanEnd = (Iban)applicationContext.getBean("Iban");
+		ibanEnd = (Iban) applicationContext.getBean("Iban");
 	}
 }
