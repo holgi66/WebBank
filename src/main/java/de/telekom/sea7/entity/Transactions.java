e4b769a331ced8ff0e026bb49d0c056e9dabc0eb
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-03 08:13:40 +0100

    20220303 Start

--- src/main/java/de/telekom/sea7/entity/Transactions.java
+++ src/main/java/de/telekom/sea7/entity/Transactions.java
@@ -23,7 +23,7 @@ public class Transactions {
 	private BigDecimal amount;
 	private String purpose;
 	private Timestamp date;
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -59,20 +59,16 @@ public class Transactions {
 	@ManyToOne
 	@JoinColumn(name = "iban_id")
 	private String iban;
-	
+
 	public Iban getIban() {
 		Iban iban = IbanRepository.findById(1L).get();
 		return iban;
 	}
-	
-//	public void setIban(String iban) {
-//		this.iban = iban;
-//	}
 
 	@ManyToOne
 	@JoinColumn(name = "receiver_id")
-    private String receiver;
-	
+	private String receiver;
+
 	public Receiver getReceiver() {
 		Receiver receiver = ReceiverRepository.findById(1L).get();
 		return receiver;
@@ -81,10 +77,8 @@ public class Transactions {
 //	public void setReceiver(String receiver) {
 //		this.receiver = receiver;
 //	}
-	
+
 	public Transactions() {
 	}
-
-
 
 }
