7f06690aec09445b73219c98959dcb058c371128
holgi66 <holger-strohmeyer_stdent2021@wilder.school>
2022-03-03 11:38:00 +0100

    20220303 Nutzung Spring Boot Bean Life Cycle mit IbanBean.java

---
+++ src/main/java/de/telekom/sea7/entity/IbanBean.java
@@ -0,0 +1,41 @@
+package de.telekom.sea7.entity;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IbanBean implements ApplicationContextAware {
+
+	@Autowired private Iban iban;
+	
+	private Iban ibanEnd;
+	
+	private ApplicationContext applicationContext;
+	
+	public IbanBean() {
+		System.out.println(iban);
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("#### IbanBean startet ####");
+		System.out.println(iban);
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("#### IbanBean endet ####");
+	}
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+		ibanEnd = (Iban)applicationContext.getBean("Iban");
+	}
+}
